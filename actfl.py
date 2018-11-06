8a541d070e06d6bcc83615257d258eb1def54566
Michael <tsz.wong753@gmail.com>
2018-11-06 16:56:48 -0500

    Finished sample creation, need to complete pardot form

--- actfl.py
+++ actfl.py
@@ -66,8 +66,9 @@ def map_from_to(v, n):
 
     for v in all_val:
         curr_tag_name = n.get(v)
-        if curr_tag_name is not None or curr_tag_name is not '':
-            ret_val[curr_tag_name] = curr_tag_name.split("_")[-1]
+        if curr_tag_name is not None:
+            if curr_tag_name is not '':
+                ret_val[curr_tag_name] = curr_tag_name.split("_")[-1]
 
     return ret_val
 
@@ -146,10 +147,10 @@ def handle_headers(header, value):
     # This is actually DIGITAL TOOLS because Xpress Leads is terrible.
     if header == "The next adoption-related deadline will be":
         digital_tools_values = {
-            '1 to 1': '',
-            '1 to 2 or more': '',
-            'Technology Cart': '',
-            'Language Lab': ''
+            '1 to 1': '359661_41983pi_359661_41983_426031',
+            '1 to 2 or more': '359661_41983pi_359661_41983_426033',
+            'Technology Cart': '359661_41983pi_359661_41983_426035',
+            'Language Lab': '359661_41983pi_359661_41983_426401'
         }
 
         return map_from_to(value, digital_tools_values)
@@ -167,13 +168,13 @@ def handle_headers(header, value):
 
     if header == 'Wayside should stay in touch about':
         in_touch_values = {
-            'Wayside newsletter subscription': '',
-            'EntreCultures 1a 1b - French': '',
+            'Wayside newsletter subscription': '359661_41959pi_359661_41959_426437',
+            'EntreCultures 1a 1b - French': '359661_41963pi_359661_41963_425983',
             'EntreCultures 1  - French': '',
             'EntreCultures 2 and or 3 - French': '',
-            'Updates on EntreCulturas 4 - Spanish': '',
-            'Learning Site Updates': '',
-            'Updates on German 1, 2, 3': ''
+            'Updates on EntreCulturas 4 - Spanish': '359661_42127pi_359661_42127_426439',
+            'Learning Site Updates': '359661_42129pi_359661_42129_426441',
+            'Updates on German 1, 2, 3': '359661_41965pi_359661_41965_425985'
         }
 
         return map_from_to(value, in_touch_values)
@@ -181,20 +182,20 @@ def handle_headers(header, value):
     # This is actually POST-ACTFL SHIPMENT REQUESTED
     if header == 'If you had your wish, Wayside would create':
         create_values = {
-            'EntreCulturas 1a': '',
-            'EntreCulturas 1b': '',
-            'EntreCulturas 1': '',
-            'EntreCulturas 2': '',
-            'EntreCulturas 3': '',
-            'Tejidos': '',
-            'Triangulo Aprobado': '',
-            'Triangulo APreciado': '',
-            'Azulejo': '',
-            'APprenons': '',
-            'Neue Blickwinkel': '',
-            'Chiarissimo Uno': '',
-            'Chiarissimo Due': '',
-            'Scandite Muros': ''
+            'EntreCulturas 1a': '359661_42135pi_359661_42135_426447',
+            'EntreCulturas 1b': '359661_42137pi_359661_42137_426449',
+            'EntreCulturas 1': '359661_42139pi_359661_42139_426451',
+            'EntreCulturas 2': '359661_42141pi_359661_42141_426453',
+            'EntreCulturas 3': '359661_42143pi_359661_42143_426455',
+            'Tejidos': '359661_42145pi_359661_42145_426457',
+            'Triangulo Aprobado': '359661_42147pi_359661_42147_426459',
+            'Triangulo APreciado': '359661_42149pi_359661_42149_426461',
+            'Azulejo': '359661_44005pi_359661_44005_444589',
+            'APprenons': '359661_42153pi_359661_42153_426465',
+            'Neue Blickwinkel': '359661_42155pi_359661_42155_426467',
+            'Chiarissimo Uno': '359661_42157pi_359661_42157_426469',
+            'Chiarissimo Due': '359661_42159pi_359661_42159_426471',
+            'Scandite Muros': '359661_42161pi_359661_42161_426473'
         }
 
         return map_from_to(value, create_values)
@@ -202,20 +203,28 @@ def handle_headers(header, value):
     # This is actually IF YOU HAD YOUR WISH, WAYSIDE WOULD CREATE
     if header == 'OPTIONAL Non Sales Follow Up':
         sales_values = {
-            'Spanish for heritage speakers': '',
-            'Elementary Spanish': '',
-            'Chiarissimo Tre': '',
-            'Elementary French': '',
-            'French for heritage speakers': '',
-            'Elementary Mandarin': '',
-            '6-12 Mandarin': '',
-            'A broader Latin line': ''
+            'Chiarissimo Tre': '359661_41967pi_359661_41967_425987',
+            'Elementary French': '359661_41969pi_359661_41969_425989',
+            'Elementary German': '359661_41971pi_359661_41971_425991',
+            'Elementary Spanish': '359661_41975pi_359661_41975_425995',
+            'Elementary Mandarin': '359661_42133pi_359661_42133_426445',
+            'Mandarin 1, 2, 3': '359661_41977pi_359661_41977_425997',
         }
 
         return map_from_to(value, sales_values)
 
+    if header == 'Lead Rating':
+        rating_values = {
+            '1': '359661_41993pi_359661_41993_426403',
+            '2': '359661_41993pi_359661_41993_426405',
+            '3': '359661_41993pi_359661_41993_426407',
+            '4': '359661_41993pi_359661_41993_426409',
+            '5': '359661_41993pi_359661_41993_426411'
+        }
+        return map_from_to(value, rating_values)
 
-def push_data_to(fn, url):
+
+def push_data_to(fn, url_pardot):
     with open(fn, 'r') as csvfile:
         reader = csv.DictReader(csvfile)
         headers = reader.fieldnames
@@ -257,15 +266,15 @@ def push_data_to(fn, url):
             # This is actually IF YOU HAD YOUR WISH, WAYSIDE WOULD CREATE
             'OPTIONAL Non Sales Follow Up': '',
 
-            'Print': '21',
-            'Ship': '22',
-            'Lead_Rating': '23',
+            'Notes': '359661_41991pi_359661_41991',
+
+            'Lead_Rating': '',
         }
 
         all_data = []
 
         for row in reader:
-            data = {}
+            pardot_data = {}
             for header in headers:
                 tag_name = tag_names.get(header)
                 if tag_name is not None:
@@ -278,22 +287,95 @@ def push_data_to(fn, url):
                                   "Digital tools in your classroom",
                                   "Wayside should stay in touch about",
                                   "If you had your wish, Wayside would create",
-                                  "Post-ACTFL shipment requested",
                                   "Lead Rating"]:
                         return_value = handle_headers(header, value)
                         if isinstance(return_value, str):
-                            data[tag_name] = return_value
+                            pardot_data[tag_name] = return_value
                         else:
-                            data.update(return_value)
+                            pardot_data.update(return_value)
                     else:
-                        data[tag_name] = value
-            all_data.append(data)
+                        pardot_data[tag_name] = value
+            all_data.append(pardot_data)
 
         # Enter POST request here
-
-        for data in all_data[2:]:
-            r = requests.post(url, data=data)
+        for data in all_data[-1:]:
+            r = requests.post(url_pardot, data=data)
             print(r.status_code)
+
+
+def push_data_to_ls(fn, url_ls):
+    with open(fn, 'r') as csvfile:
+        all_data = []
+        reader = csv.DictReader(csvfile)
+        headers = reader.fieldnames
+
+        for row in reader:
+            data = {
+                'key': "EOQ89SMVP3K",
+                'campaign': "email",
+                'textbook': [],
+                'email': "",
+                'firstname': "",
+                'lastname': "",
+                'school': "",
+                'city': "",
+                'state': "",
+                'zipCode': ""
+            }
+
+            for header in headers:
+                curr_val = row[header]
+
+                if header == 'Email 30 day access to these programs':
+                    all_textbooks = curr_val.split("|")
+                    textbook_ids = {
+                        'EntreCultures Sampler': '',
+                        'EntreCulturas 1a': '',
+                        'EntreCulturas 1b': '',
+                        'EntreCulturas 1': '13463',
+                        'EntreCulturas 2': '13507',
+                        'EntreCulturas 3': '13518',
+                        'Tejidos': '1955',
+                        'Triangulo Aprobado': '3551',
+                        'Triangulo APreciado': '',
+                        'Azulejo': '3',
+                        'APprenons': '11138',
+                        'Neue Blickwinkel': '12741',
+                        'Chiarissimo Uno': '6698',
+                        'Chiarissimo Due': '12271',
+                        'Scandite Muros': '13435',
+                    }
+
+                    for t in all_textbooks:
+                        curr_id = textbook_ids.get(t)
+                        if curr_id is not None and curr_id is not '':
+                            data['textbook'].append(curr_id)
+
+                if header == 'Email':
+                    data['email'] = curr_val
+
+                if header == 'First Name':
+                    data['firstname'] = curr_val
+
+                if header == 'Last Name':
+                    data['lastname'] = curr_val
+
+                if header == 'Company':
+                    data['school'] = curr_val
+
+                if header == 'City':
+                    data['city'] = curr_val
+
+                if header == 'State/Province':
+                    data['state'] = curr_val
+
+                if header == 'Zip Code':
+                    data['zipCode'] = curr_val
+
+            all_data.append(data)
+    for d in all_data[-1:]:
+        r = requests.post(url_ls, data=d)
+        print(r.text)
 
 
 def cleanup():
@@ -312,10 +394,14 @@ def main():
     get_conf_data()
     
     # Parsing through new data to check for duplicates between "new_data.csv" and "archive.csv" if "archive.csv" exists
+    # Returns file name to process
     parsed_data_fn = get_just_new_data_from()
     
-    # Push data to the LS and Pardot
+    # Push data to Pardot
     push_data_to(parsed_data_fn, 'http://www2.waysidepublishing.com/l/359661/2018-10-22/dn4z2b')
+
+    # Push data to the LS
+    push_data_to_ls(parsed_data_fn, 'https://learningsite.waysidepublishing.com/api/user/')
 
     # Deleting "archive.csv" and "delta.csv" if necessary, renames "new_data.csv" to "archive.csv"
     cleanup()   
